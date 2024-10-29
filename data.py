a332f02507e7d9f6b07100ee8f2ec66ed8b2f0d7
jhfreita <jhfreita@calpoly.edu>
2024-10-28 22:39:33 -0700

    Update data.py

--- data.py
+++ data.py
@@ -24,7 +24,29 @@ class Time:
     # input: Another value to compare to the Time
     # output: boolean indicating equality
 
+    # The following function takes another time object as the input and returns True if the time
+    # object is equal to the given time object and false if the object is not equal to
+    # the given time object.
+    def __eq__(self, other):
+        return type(other) == Time and self.hour == other.hour and self.minute == other.minute and self.second == other.second
 
+# The following function takes the current time object being created as the input and
+# returns a string representation of the time in terms of hours, minutes, and seconds as the output.
+    def __repr__(self):
+        return ("hours: {}, minutes: {}, seconds: {}".format(self.hour, self.minute, self.second))
+
+def test_eq_Time1(self, other):
+   Time1 = Time(4, 12, 42)
+   Time2 = Time(4, 12, 42)
+   self.assertEqual(Time1.hour, Time2.hour)
+   self.assertEqual(Time1.minute, Time2.minute)
+   self.assertEqual(Time1.second, Time2.second)
+
+def test_eq_Time2(self):
+    time = data.Time(2, 44, 16)
+    self.assertEqual(2, time.hour)
+    self.assertEqual(44, time.minute)
+    self.assertEqual(16, time.second)
 
 
 # Representation of a two-dimensional point.
